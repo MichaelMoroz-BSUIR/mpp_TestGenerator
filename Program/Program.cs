28dca8f3fce5312f72e1e6bee2e652cb709e81aa
MichaelMoroz-BSUIR <mishamoroz50@gmail.com>
2021-11-24 01:44:59 +0300

    Initial commit

---
+++ Program/Program.cs
@@ -0,0 +1,19 @@
+﻿using TestGenerator;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var generator = NUnitTestGenerator.CreateGenerator(builder =>
+            {
+                builder.MaxGenerateFiles = 10;
+                builder.MaxReadFiles = 10;
+                builder.MaxWriteFiles = 10;
+            });
+            generator.GenerateTestsAsync(@"C:\Users\misha\RiderProjects\mpp_TestGenerator\Program\input",
+                @"C:\Users\misha\RiderProjects\mpp_TestGenerator\Program\output").Wait();
+        }
+    }
+}
